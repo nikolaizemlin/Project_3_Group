170bd25b7d72681ff2461d9f8fda76d42a7f9d91
Thomas Duncan <thomas.a.duncan9@gmail.com>
2018-09-26 21:37:14 -0700

    added component for Instruction on brewing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import History from "./History.js";
+import HowTo from "./HowTo.js"
 
 const App = () => (
   <div>
   <History />
+  <HowTo />
+  
   </div>
 );
 
