fb54115666a380777002665c9da614d67a683432
Nick Hosley <nhosley1976@gmail.com>
2018-09-27 21:17:18 -0700

    Revert "added some files"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Blog from "./components/pages/Blog";
-import Contact from "./components/pages/Contact";
-// import formInstance from "./components/pages/formInstance";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => (
-  <Router>
-    <div>
-      <NavTabs />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/blog" component={Blog} />
-      <Route path="/contact" component={Contact} />
-      {/* <Route path="/formInstance" component={formInstance} /> */}
-    </div>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
