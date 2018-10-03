1a18e79b339bb178ae00837f2bf6dfdff1d3f96c
Nick Hosley <nhosley1976@gmail.com>
2018-10-03 14:39:54 -0700

    addeds stuff for clicking

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,87 @@
 import React from "react";
+import { Carousel, Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap';
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+ 
+ <Carousel>
+  <Carousel.Item>
+    <img width='100%' height={400} alt="900x500" src="img/beerglasses.jpg" />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width='100%' height={400} alt="900x500" src="img/blackandwhite.jpg" />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width='100%' height={400} alt="900x500" src="img/wars.jpg" />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width='100%' height={400} alt="900x500" src="img/123877.jpg" />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>;
+
+<Grid>
+  <Row>
+    <Col xs={6} md={3}>
+      <Thumbnail src="\img\nick.jpg" alt="242x200">
+        <h3>Nick Hosley</h3>
+        <p>Typed some Letters and symbols</p>
+        <p>
+          <Button bsStyle="primary" href="https://nikolaizemlin.github.io/Portfolio/" target="_blank">Button</Button>
+          
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail src="/img/meg.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>
+          &nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail src="/img/kenzie.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>
+          &nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail src="/img/andy.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>
+          &nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
   </div>
 );
 
