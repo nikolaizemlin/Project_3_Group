a170a539fb8c42b3b0cc299cd8f73ced3606c86d
Thomas Duncan <thomas.a.duncan9@gmail.com>
2018-10-04 13:54:09 -0700

    added header and footer

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Styles.css'
+
+const Footer = () => (
+    <footer>&copy; 2018 </footer>
+);
+
+export default Footer;
---
+++ src/components/pages/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Styles.css'
+
+
+const Header = () => (
+<div>
+<header>
+<h1 id="name">Home Brewing at its Best</h1>
+<h2 id="moto">Making Your Brewing Dreams Come True!</h2>
+
+</header>
+<body>
+
+    
+    
+<div class="top"></div>
+
+
+
+</body>
+
+</div>
+
+
+)
+
+export default Header;
